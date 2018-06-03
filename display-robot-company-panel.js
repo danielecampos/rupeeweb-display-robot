7ec68f5b5dd5d453c356a23501ff6cd17f74224f
developer <jairo.araujo.silva@gmail.com>
2018-06-03 16:26:20 -0300

    .

--- display-robot-company-panel.js
+++ display-robot-company-panel.js
@@ -2,27 +2,30 @@
 
 (function display_robot_company_panel(interval) {
     console.log('running display_robot_company_panel');
-    var companies = document.querySelectorAll('.companies-container a');
-    var companies_url = [];
-    for (let c of companies)
-        companies_url.push(c.href);        
     var last_window_pageyoffset = -1;
     var g = () => {
-        var reachedbottom = ((window.pageYOffset - last_window_pageyoffset) == 0);
-        if (!reachedbottom) {
-            console.log(window.pageYOffset, last_window_pageyoffset);
-            last_window_pageyoffset = window.pageYOffset;
-            window.scrollBy(0, 8);     
-        } else {
-            console.log('reached bottom');
+        try {
+            var reachedbottom = ((window.pageYOffset - last_window_pageyoffset) == 0);
+            if (!reachedbottom) {
+                last_window_pageyoffset = window.pageYOffset;
+                window.scrollBy(0, 8);     
+            } else {
+                clearInterval(drcp_interval);
+                var companies = document.querySelectorAll('.companies-container a');
+                var companies_url = [];
+                for (let c of companies)
+                    companies_url.push(c.href);        
+                var current_url_ix = companies_url.lastIndexOf(window.location.href);
+                var next_url;
+                if (current_url_ix === companies_url.length-1) 
+                    next_url = 'https://www.rupee.com.br/dashboard_grecia'
+                else
+                    next_url = companies_url[current_url_ix+1];
+                setTimeout(() => window.location.href = next_url, interval);
+            }
+        } catch (err) {
             clearInterval(drcp_interval);
-            var current_url_ix = companies_url.lastIndexOf(window.location.href);
-            var next_url;
-            if (current_url_ix === companies_url.length-1) 
-                next_url = 'https://www.rupee.com.br/dashboard_grecia'
-            else
-                next_url = companies_url[current_url_ix+1];
-            setTimeout(() => window.location.href = next_url, interval);
+            throw err;
         }
     };
     var drcp_interval = setInterval(g, 200);
